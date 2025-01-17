2e9089532bd1116511abae181280a8d2d4fe0dc1
Andrew Trepagnier <160799843+AndrewTrepagnier@users.noreply.github.com>
2025-01-16 18:02:55 -0600

    Update noisy-sine-wave-gen.py

--- noisy-sine-wave-gen.py
+++ noisy-sine-wave-gen.py
@@ -1,23 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Generate x values for exactly two oscillations
+
 #random_weight_x = np.round(np.random.uniform(low=0, high=2, size=10), 1)  # Changed high to 2
-clean_array_x = np.linspace(0, 1, 100)  # Changed high to 2
+clean_array_x = np.linspace(0, 10, 100)  # Changed high to 2
 
 random_array_x = np.zeros(10)
 
 for i in range(10):
-    random_array_x[i] =  i/8 + np.random.random(size=1)
+    random_array_x[i] =  i + np.random.random(size=1)
 print(random_array_x)
     
 # Create sine wave functions
 def noisy_sine(random_array_x):
-    random_weight_y = 1.4*np.round(np.random.uniform(low = -1, high = 1, size = len(random_array_x)),1)
-    return 5*np.sin(2*np.pi*random_array_x) + random_weight_y 
+    random_weight_y = 1.4*np.round(np.random.uniform(low = -3, high = 3, size = len(random_array_x)),1)
+    return 20*np.sin(2*np.pi*random_array_x/10) + random_weight_y 
 
 def clean_sine(clean_array_x):
-    return 5*np.sin(2*np.pi*clean_array_x)  
+    return 20*np.sin(2*np.pi*clean_array_x/10)  
 
 
 y_noise = noisy_sine(random_array_x)
